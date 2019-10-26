cae3e70fa576d084cf0869a4d02880ebb09be7eb
JUN1113 <dunkelrots@gmail.com>
2019-10-27 02:10:51 +0900

    お問い合わせフォームとTwitterへのリンク

---
+++ resources/views/contact/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+@section('content')
+<div class="contactIndex">
+    <a href="/contact/create">お問合せフォーム</a>
+    <a href="https://twitter.com/JUN_program" target="_blank">Twitterでのお問い合わせ</a>
+</div>
+@endsection
