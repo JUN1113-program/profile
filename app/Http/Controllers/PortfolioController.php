15f098b1612b6541f1ce431c9021ae19c008849e
JUN1113 <dunkelrots@gmail.com>
2019-10-26 15:26:20 +0900

    Create PortfolioController.php

---
+++ app/Http/Controllers/PortfolioController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Portfolio;
+
+class PortfolioController extends Controller
+{
+}
