d1d508c4cf70fc7e04d011b3ddbf1ae205d33379
JUN1113 <dunkelrots@gmail.com>
2019-10-26 19:04:05 +0900

    iconモデルへのリレーション

--- app/User.php
+++ app/User.php
@@ -44,4 +44,8 @@ class User extends Authenticatable
     public function profile(){
         return $this->hasOne("App\Profile");
     }
+
+    public function icon(){
+        return $this->hasOne("App\Icon");
+    }
 }
