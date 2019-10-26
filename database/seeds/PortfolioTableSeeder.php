b8ca13a6c655f2f589378ed1291d76caced042c2
JUN1113 <dunkelrots@gmail.com>
2019-10-26 15:29:26 +0900

    portfolioのseedの追加

--- database/seeds/PortfolioTableSeeder.php
+++ database/seeds/PortfolioTableSeeder.php
@@ -11,14 +11,34 @@ class PortfolioTableSeeder extends Seeder
      */
     public function run()
     {
-        $portfolio =    [
+        $portfolios = [
+                        [
                         "title" => "voCab",
                         "explanation" => "単語帳アプリ",
                         "env" => "Rails, JavaScript, Haml, SCSS, MySQL, heroku",
                         "url" => "https://vo-cab.herokuapp.com/",
                         "src" => "https://github.com/JUN1113-program/voCab",
                         "user_id" => 1
-                        ];
-        \App\Portfolio::create($portfolio);
+                        ],
+                        [
+                        "title" => "MiniWebLog",
+                        "explanation" => "簡単な画像投稿ブログ",
+                        "env" => "Rails, Haml, SCSS, MySQL, heroku",
+                        "url" => "https://miniweblog.herokuapp.com/",
+                        "src" => "https://github.com/JUN1113-program/mini-web-log",
+                        "user_id" => 1
+                        ],
+                        [
+                        "title" => "王様Kingdom",
+                        "explanation" => "プレイヤーが王様になって国民を派遣するシミュレーションPRG",
+                        "env" => "Java, jFrame, jPanel, Eclipse",
+                        "url" => "",
+                        "src" => "https://github.com/JUN1113-program/Kingdom",
+                        "user_id" => 1
+                        ]
+                    ];
+        foreach($portfolios as $portfolio){
+            \App\Portfolio::create($portfolio);
+        }
     }
 }
