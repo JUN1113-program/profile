a372d0a79f067e8813bb3cdc8e5df12d564dccce
JUN1113 <dunkelrots@gmail.com>
2019-10-26 15:28:05 +0900

    マイグレーションファイルを複数形に

--- database/migrations/2019_10_25_184330_create_portfolio_table.php
+++ database/migrations/2019_10_25_184330_create_portfolio_table.php
@@ -33,6 +33,6 @@ class CreatePortfolioTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('portfolio');
+        Schema::dropIfExists('portfolios');
     }
 }
