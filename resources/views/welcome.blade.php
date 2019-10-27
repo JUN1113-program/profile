5eb0a48c2b53f0dd2b8d2bc5845851eaec0397bb
JUN1113 <dunkelrots@gmail.com>
2019-10-27 15:37:50 +0900

    topページからユーザー登録への移動を禁止

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,5 @@
 @extends('layout')
 @section('content')
-@if (Route::has('login'))
-    <div class="top-right links">
-        @auth
-            <a href="{{ url('/home') }}">Home</a>
-        @else
-            <a href="{{ route('login') }}">Login</a>
-
-            @if (Route::has('register'))
-                <a href="{{ route('register') }}">Register</a>
-            @endif
-        @endauth
-    </div>
-@endif
 <div class="top">
     <a href="/portfolio" class="top__item">
         PORTFOLIO
