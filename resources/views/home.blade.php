f66d33fc09f3224768e1697b9fa346f56787f397
JUN1113 <dunkelrots@gmail.com>
2019-10-27 19:25:34 +0900

    contactをhomeに追加

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,18 +6,29 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
-
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-
                     You are logged in!
                 </div>
             </div>
         </div>
+        @foreach ($contacts as $contact)
+            <div class="mt-3 col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        Contact from {{$contact->email}}
+                        <button class="close" type="button" onclick="location.href=''">&times;</button>
+                    </div>
+                    <div class="card-body">
+                        {{$contact->content}}
+                    </div>
+                </div>
+            </div>
+        @endforeach
     </div>
 </div>
 @endsection
