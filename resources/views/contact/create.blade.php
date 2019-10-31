666587f65b4ede27932dc08c9cc312da977ebbd3
JUN1113 <dunkelrots@gmail.com>
2019-10-31 14:01:28 +0900

    contactの送信フォームをbootstrap化

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -1,13 +1,19 @@
 @extends('layout')
 @section('content')
-<div class="contactCreate">
-    <form action="/contact" method="post" class="contactCreate__form">
-        {{ csrf_field() }}
-        <label for="contact__content">お問い合わせ内容 </label>
-        <textarea class="contactCreate__form__input" name="content" id="contact__content" cols="30" rows="10" required autofocus></textarea>
-        <label for="contact__email">返信用アドレス</label>
-        <input class="contactCreate__form__input" type="email" name="email" id="contact__email">
-        <input class="contactCreate__form__submit "type="submit" value="送信する">
-    </form>
+<div class="container mt-5">
+    <div class="col-md-8 mx-auto">
+        <form action="/contact/" method="post" class="contactCreate__form">
+            {{ csrf_field() }}
+            <div class="form-group">
+                    <label for="contactEmail">返信用アドレス</label>
+                    <input class="form-control" type="email" name="email" id="contactEmail">
+                </div>
+            <div class="form-group">
+                <label for="contactContent">お問い合わせ内容</label>
+                <textarea class="form-control" name="content" id="contactContent" cols="30" rows="10" required autofocus></textarea>
+            </div>
+            <input class="btn btn-outline-primary btn-block" type="submit" value="問い合わせる">
+        </form>
+    </div>
 </div>
 @endsection
