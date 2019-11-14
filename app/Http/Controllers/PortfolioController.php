31e9b793b96e406dedc74ee0f82d59afd6d8fa47
JUN1113 <dunkelrots@gmail.com>
2019-11-14 18:10:43 +0900

    indexアクション以外にauth

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -9,6 +9,11 @@ use App\Portfolio;
 
 class PortfolioController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->except(["index"]);
+    }
+
     public function index(){
         $portfolios = Portfolio::all();
 
